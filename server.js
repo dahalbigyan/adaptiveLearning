a9f8f042a314c86e6b9890be6164b5d4555cf02f
bigyan.dahal@knovva.com <bigyan.dahal@knovva.com>
2018-03-29 14:29:14 -0400

    Added body parser.

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const cors = require('cors');
 const app = express();
 const port = 8000;
 const routes = require('./router');
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
 // app.use('/', routes);
 
@@ -16,12 +18,14 @@ app.get("/statements", (req, res)=>{
   res.send("Calling from the statements route.");
 });
 app.put("/statements", (req, res)=>{
+  console.log(req.body);
   console.log("Calling from the put statements route.");
   res.send("Calling from the statements route.");
 });
 
 app.get("/activities/state", (req, res)=>{
   console.log("Calling from the activities/state route.");
+  console.log(req.body);
   res.send("Calling from the activities/state route.");
 });
 
