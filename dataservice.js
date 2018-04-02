83e39beea8f17ee9efa68a3f549785076ce7c5fe
bigyan.dahal@knovva.com <bigyan.dahal@knovva.com>
2018-04-02 14:09:58 -0400

    More updates.

--- dataservice.js
+++ dataservice.js
@@ -1,11 +1,30 @@
-
+const request = require('graphql-request').request;
+const GraphQLClient = require('graphql-request').GraphQLClient;
+const endpoint = `https://api.graph.cool/simple/v1/cje4lfqbv55gk01570mjrcd44`;
 const arr = [];
 
 function saveDataToTheGraphCool(data){
-  arr.push(data);
-  console.log(arr.length);
-};
+    // console.log(data);
+  // const mutation = `{
+  //   createScorm($content:Json!){
+  //     content: $content
+  //   }{
+  //     id
+  //   };
+  // }`;
+  //
+  // const variables = {
+  //   content: JSON.stringify(data)
+  // }
 
+  const query = `{
+  allMovies{
+    id,
+    avgRating
+  }
+}`;
+  request(endpoint, query).then(data=>console.log(data)).catch(error=>console.log(error));
+};
 
 
 module.exports = {
