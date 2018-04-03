73978e48c08f8593f92f07494a5d265af24e9554
bigyan.dahal@knovva.com <bigyan.dahal@knovva.com>
2018-04-03 11:04:38 -0400

    Additional changes.

--- dataservice.js
+++ dataservice.js
@@ -6,25 +6,24 @@ function getUseridAndContent(data){
   const actorLength = data.actor.name.length;
   actorid = data.actor.name.slice(actorLength-25, actorLength);
   const actorname = data.actor.name.slice(0, actorLength-25);
-  const userData={...data};
-  userData.name = actorname;
-  return {userData, actorid};
+  return {actorname, actorid};
 };
 
-function saveDataToTheGraphCool(data
+function saveDataToTheGraphCool(data){
+  const {actorname, actorid} = getUseridAndContent(data);
+  data.actor.name = actorname;
   const mutation = `mutation createScorm($content: Json!){
     createScorm(content: $content){
-      id,
-      content
+      id
     }
   }
   `;
 
   const variables = {
-    content: responseObjectAfterIdRemoval
-  }
+    content: data
+  };
 
-  request(endpoint, mutation, variables).then(data=>console.log(data)).catch(error=>console.log(error));
+  request(endpoint, mutation, variables).then(data=>console.log(data)).catch(error=>console.error(error));
 };
 
 
