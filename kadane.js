3a4312a3b43aa43cba6e410e901d27cde32cc0b4
bigyan.dahal@knovva.com <bigyan.dahal@knovva.com>
2018-05-15 19:37:08 -0400

    Addition of kadane algorithm.

---
+++ kadane.js
@@ -0,0 +1,33 @@
+
+const arr = [-5,6,7,1,4,-8,16];
+
+function findMaxSum(arr){
+  let currentMaxSum = 0;
+  let nextMaxSum = 0;
+  for(let i = 0; i<arr.length; i++){
+    nextMaxSum = 0;
+    for(let j = i; j<arr.length; j++){
+      nextMaxSum += arr[j];
+      if(nextMaxSum>currentMaxSum){
+        currentMaxSum = nextMaxSum;
+      }
+    };
+  };
+  return currentMaxSum;
+};
+console.log(findMaxSum(arr));
+
+function findMaxSumDynamic(arr){
+  let currentMaxSum;
+  let globalMaxSum;
+  currentMaxSum = globalMaxSum = arr[0];
+  for(let i = 1; i<arr.length; i++){
+    currentMaxSum = Math.max(arr[i], currentMaxSum + arr[i]);
+  };
+  if(currentMaxSum > globalMaxSum){
+    globalMaxSum = currentMaxSum;
+  };
+  return globalMaxSum;
+};
+
+console.log(findMaxSumDynamic(arr));
